3a84957184cccdebecada7d2897dac59f4e5ae5e
benrus200 <62921585+benrus200@users.noreply.github.com>
2020-09-21 22:33:10 -0400

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,22 @@
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+File <- "./Data/household_power_consumption.zip"
+householdFile <- "./Data/household_power_consumption.txt"
+
+##
+if (!file.exists(householdFile)) {
+  download.file(URL, File, method = "curl")
+  unzip(File, overwrite = T, exdir = "./Data")
+}
+
+##
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+
+## Generating Plot 2
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
